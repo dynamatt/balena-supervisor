1a2e798e2eefcb4dff7cdc7903d4fe3d86085639
Matt Williams <mattmwilliams89@gmail.com>
2020-04-06 21:54:23 +1000

    Created directory structure.

---
+++ src/Balena.Supervisor/BalenaSupervisorConnection.cs
@@ -0,0 +1,17 @@
+﻿namespace Balena.Supervisor
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using RestSharp;
+    using RestSharp.Authenticators;
+
+    public class BalenaSupervisorConnection
+    {
+        private readonly RestClient client;
+
+        public BalenaSupervisorConnection()
+        {
+
+        }
+    }
+}
